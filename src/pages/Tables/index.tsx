76cd85ca3576f505d5e476781d28e610fe339547
clebercmsf <cleber.cmsf@gmail.com>
2024-05-29 15:56:55 -0300

    tables-start

--- src/pages/Tables/index.tsx
+++ src/pages/Tables/index.tsx
@@ -1,5 +1,23 @@
+import "./styles.scss";
+import database from "../../databases/prices.json";
+
 export default function Tables () {
   return (
-    <></>
+    <section className="tables">
+      <div className="tables-header">
+        <h4>Nome</h4>
+        <h4>Valor Unidade</h4>
+        <h4>Valor Pack</h4>
+      </div>
+      <div className="tables-content">
+        {database.ores.map(item => (
+          <div key={item.id}>
+            <span>{item.name}</span>
+            <span>{item.unit}</span>
+            <span>{item.pack}</span>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }

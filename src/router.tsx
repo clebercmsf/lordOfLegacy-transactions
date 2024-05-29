b7e0200fc34d4fb65e66ec03cf7c1177030d7204
clebercmsf <cleber.cmsf@gmail.com>
2024-05-29 15:39:47 -0300

    tables-start

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "./components/RootLayout";
 import Calculator from "./pages/Calculator";
+import Tables from "./pages/Tables";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Calculator />
+      },
+      {
+        path: "tabelas",
+        element: <Tables />
       }
     ]
   }
